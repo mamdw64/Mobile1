5c16721216ec656da246abffe9739024f5bb0228
Hirad8520 <amirrezaj37@gmail.com>
2023-06-11 10:12:10 +0330

    first Frootani

--- app/src/main/java/com/example/bottonnavig/MainActivity.kt
+++ app/src/main/java/com/example/bottonnavig/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   myNavigation()
                 }
             }
         }
@@ -30,17 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun myNavigation() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BottonNavigTheme {
-        Greeting("Android")
-    }
-}
+
+        }

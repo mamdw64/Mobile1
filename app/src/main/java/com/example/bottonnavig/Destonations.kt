21e1c534c37f3559923671bc44320312109ea655
Hirad8520 <amirrezaj37@gmail.com>
2023-06-11 10:16:43 +0330

    second miiad

---
+++ app/src/main/java/com/example/bottonnavig/Destonations.kt
@@ -0,0 +1,15 @@
+package com.example.bottonnavig
+
+interface Destonations
+{
+    val Route :String
+}
+
+object Home :Destonations
+{
+    override val Route ="Home"
+}
+object Second:Destonations
+{
+    override val Route ="Second"
+}
